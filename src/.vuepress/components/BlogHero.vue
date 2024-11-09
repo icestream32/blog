73fa356c73aeac3f3bc791d6000bc135dc091656
icestream32 <1601334224@qq.com>
2024-11-09 12:58:39 +0800

    feat(project): 将首页壁纸改为必应每日壁纸

--- src/.vuepress/components/BlogHero.vue
+++ src/.vuepress/components/BlogHero.vue
@@ -2,12 +2,16 @@
 <script setup lang="ts">
 import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
 import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
 </script>
 
 <template>
     <BlogHero>
         <template #info="info">
             <HitokotoBlogHero v-bind="info" />
+        </template>
+        <template #bg>
+            <BingHeroBackground />
         </template>
     </BlogHero>
 </template>
